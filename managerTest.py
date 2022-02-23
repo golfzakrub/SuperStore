14a268b8dd0961497c71a6f3cb23b2db684a8ca8
golfzakrub <70587016+golfzakrub@users.noreply.github.com>
2022-02-24 00:45:45 +0700

    add/ 3 test

--- managerTest.py
+++ managerTest.py
@@ -1,7 +1,7 @@
 import  pandas as pd
 import os
 import json
-
+import hashlib
 class Manager():
     def readData(self,file):
         self.all_data = pd.read_csv(file,encoding = 'windows-1252').dropna()
@@ -36,7 +36,10 @@ class Manager():
             all_data[str(Date_list[x])+" Month"].astype(int)
             all_data[str(Date_list[x])+" Year"].astype(int)  
         
-        return all_data
+        if len(Date_list) > 0:            
+            return True           
+        else:
+            return False
 
     def get_backup(self,file):
         if os.path.exists(file) == False:
@@ -44,7 +47,31 @@ class Manager():
         with open(file,'r') as head_file:
             self.data_head = json.load(head_file)
         return self.data_head
+    
+    def getDataFilter(self,data,item): ##recive DataFilter from filterComplete
+        filter_key = {}        
+        filter_key[item] = data
+        if len(filter_key[item]) > 0:
+            return True
+        else:
+            return False  
 
-###
+    def readUnionData(self,filename,filename_union):
+        if filename == "":
+            return print("ERROR IMPORT FILE")
+        self.data_1 = pd.read_csv(filename,encoding = 'windows-1252').dropna()
+        self.data_2 = pd.read_csv(filename_union,encoding = 'windows-1252').dropna()
+        if len(self.data_1.columns) == len(self.data_2.columns):
+            for i in range(len(self.data_1.columns)):
+                if self.data_1.columns[i] == self.data_2.columns[i] :
+                    self.all_data = pd.concat([self.data_1,self.data_2],axis=0).drop_duplicates().reset_index(drop=True)
+                    # self.showdata_head()
+                    # self.showdata_table()
+                    return len(self.all_data.index)
 
-
+    def check_md5(self,namefile,filename,data_head):
+        self.data_head = data_head
+        if self.data_head[namefile]["md5"] == hashlib.md5(open(filename,'rb').read()).hexdigest():
+            return True
+        else:
+            return False
